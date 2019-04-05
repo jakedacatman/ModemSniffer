65d3ede5df726eb27ec304ffd83f675148033b5c
jakedacatman <32282272+jakedacatman@users.noreply.github.com>
2019-04-04 20:49:16 -0400

    Update sniffConfig.lua

--- external/sniffConfig.lua
+++ external/sniffConfig.lua
@@ -2,4 +2,5 @@
 	--channel = , --uncomment this line and change the channel value to what the external modem broadcasts on
 	blacklist = { }, --add channels you'd wish to blacklist here
 	origin = "", --is there an origin variable in the message? if so, filter by just that origin here
+	truncate = nil, --wanna cut messages short? put the number of chars to have
 }
