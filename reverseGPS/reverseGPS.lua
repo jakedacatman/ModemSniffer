48f36340fadf1c7aec169bd896a322f24e0d1e6d
jakedacatman <32282272+jakedacatman@users.noreply.github.com>
2019-04-06 21:48:04 -0400

    Update reverseGPS.lua

--- reverseGPS/reverseGPS.lua
+++ reverseGPS/reverseGPS.lua
@@ -1,3 +1,26 @@
 if not fs.exists("/"..shell.resolve "locate.lua") then shell.run("wget https://raw.githubusercontent.com/jakedacatman/ModemSniffer/master/reverseGPS/locate.lua /"..shell.resolve "locate.lua") 
 if not fs.exists("/"..shell.resolve "sniffConfig.lua") then shell.run("wget https://raw.githubusercontent.com/jakedacatman/ModemSniffer/master/reverseGPS/sniffConfig.lua /"..shell.resolve "sniffConfig.lua") 
 
+local l = require("/"..shell.resolve "locate.lua")
+ 
+local file = fs.open("/"..shell.resolve "sniffConfig.lua", "r")
+local config = textutils.unserialize(file.readAll())
+file.close()
+if not config or config.channels then error "delete sniffConfig.lua"
+if #config.channels > 128 then error "modems can only have 128 channels open" end
+
+local modems = peripheral.find("modem", function(name, obj) return obj.isWireless() end)
+if #modems < 4 or #modems > 4 then error "needs 4 modems to locate" end
+    
+for i = 1, #modems do
+  for v = 1, #config.channels do
+    modems[i].open(v)        
+  end
+end
+    
+while true do
+  local ev = os.pullEvent "modem_message"
+  local pos = {l.locate(ev, 5)}
+  print(pos)
+  sleep(1)
+end
