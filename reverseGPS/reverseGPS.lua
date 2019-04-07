15ce19b076d40d7fc9825830ed02cf03207f2875
jakedacatman <32282272+jakedacatman@users.noreply.github.com>
2019-04-06 21:49:02 -0400

    Update reverseGPS.lua

--- reverseGPS/reverseGPS.lua
+++ reverseGPS/reverseGPS.lua
@@ -1,5 +1,5 @@
-if not fs.exists("/"..shell.resolve "locate.lua") then shell.run("wget https://raw.githubusercontent.com/jakedacatman/ModemSniffer/master/reverseGPS/locate.lua /"..shell.resolve "locate.lua") 
-if not fs.exists("/"..shell.resolve "sniffConfig.lua") then shell.run("wget https://raw.githubusercontent.com/jakedacatman/ModemSniffer/master/reverseGPS/sniffConfig.lua /"..shell.resolve "sniffConfig.lua") 
+if not fs.exists("/"..shell.resolve "locate.lua") then shell.run("wget https://raw.githubusercontent.com/jakedacatman/ModemSniffer/master/reverseGPS/locate.lua /"..shell.resolve "locate.lua") end
+if not fs.exists("/"..shell.resolve "sniffConfig.lua") then shell.run("wget https://raw.githubusercontent.com/jakedacatman/ModemSniffer/master/reverseGPS/sniffConfig.lua /"..shell.resolve "sniffConfig.lua") end 
 
 local l = require("/"..shell.resolve "locate.lua")
  
