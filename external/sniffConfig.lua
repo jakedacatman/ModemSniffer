5350d7fc55d87b4e409f6115f118f97e317d517d
jakedacatman <32282272+jakedacatman@users.noreply.github.com>
2019-04-21 10:06:42 -0400

    Update sniffConfig.lua

--- external/sniffConfig.lua
+++ external/sniffConfig.lua
@@ -3,4 +3,5 @@
 	blacklist = { }, --add channels you'd wish to blacklist here
 	origin = "", --is there an origin variable in the message? if so, filter by just that origin here
 	truncate = nil, --wanna cut messages short? put the number of chars to have
+	delay = nil, --want to limit when new messages are received? set this to the delay
 }
